74f411f9691185f2f57b79d1823128e66a288974
Aykhan Hagverdili <aykhanhagverdili@gmail.com>
2021-04-19 15:01:01 +0400

    Added testing macros

--- Main.cpp
+++ Main.cpp
@@ -2,17 +2,23 @@
 // Created by Ayxan Haqverdili on 4/18/2021.
 //
 
+#define ENIGMA_TEST
+
 #include "Test/EnigmaTest.hpp"
 #include <cstdio>
 
+
 int main()
-try {
-	EnigmaCpp::EnigmaTest::EncryptTest();
-	EnigmaCpp::EnigmaTest::DecryptTest();
-	EnigmaCpp::EnigmaTest::PlugBoardTest();
+{
+#ifdef ENIGMA_TEST
+	try {
+		EnigmaCpp::EnigmaTest::EncryptTest();
+		EnigmaCpp::EnigmaTest::DecryptTest();
+		EnigmaCpp::EnigmaTest::PlugBoardTest();
+		std::puts("Test cases passed!");
 
-	std::puts("Test cases passed!");
-
-} catch (std::exception const& ex) {
-	std::fputs("Test failed\n", stderr);
+	} catch (std::exception const& ex) {
+		std::fputs("Test failed\n", stderr);
+	}
+#endif // ENIGMA_TEST
 }
